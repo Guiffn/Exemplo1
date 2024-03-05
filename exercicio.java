14ce3b9e403918b7ef7aea2e61fd7c2eee64d6ac
Guilherme <guiffn2005@gmail.com>
2024-03-05 17:13:42 -0300

    Mudanças

--- exercicio.java
+++ exercicio.java
@@ -7,7 +7,28 @@ public class exercicio {
      Scanner leitor = new Scanner(System.in);
 
      System.out.println("Escreva o nome do aluno");
-     
+      String s = leitor.next();
+
+      System.out.println("Escreva sua matricula: ");
+      int m = leitor.nextInt();
+
+      System.out.println("Digite nota 1 ");
+      double not1 = leitor.nextDouble();
+
+      System.out.println("Digite nota 2 ");
+      double not2 = leitor.nextDouble();
+
+      double resultado = (not1+not2)/2;
+
+
+     System.out.println("Nome: " +s); 
+     System.out.println("Matricula: " +m);
+     if(resultado >= 60){
+      System.out.println("aprovado aluno: " +s );
+     }else{
+      System.out.println("Reprovado aluno: " +s);
+     }
+     System.out.println("Nota: "+resultado);
 
    leitor.close(); }
 }
